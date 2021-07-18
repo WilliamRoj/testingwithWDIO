7e2d81037b6ed3df5a995f5682d68557564e01ed
William Rojas <william.ur@live.com>
2021-07-18 14:32:37 -0600

    ran smoke test to verify if page loads. Test passed

--- test/specs/e2e-smoke.js
+++ test/specs/e2e-smoke.js
@@ -1,8 +1,7 @@
 describe('Smoke test - loading website', () => {
     it('load website', async () => {
         await browser.url('https://www.afloresconstruction.com/')
-        await expect($(browser)).toHaveUrl('https://www.afloresconstruction.com/);
-        await expect($(browser).toHaveTitle('FLORES CONSTRUCTION: Denver Home Remodeling Company & Renovation Contractors'))
+        await expect($('#stickyHeaderSpacer')).toBeExisting();
     })
 })
 

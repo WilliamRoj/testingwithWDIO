1f461466e112b06bc61b4d101036280928a07d2c
William Rojas <william.ur@live.com>
2021-08-03 21:27:05 -0600

    Created new heartApr.js file under page objects. created code and import to smoke-test.js. ran test passed

--- test/specs/smoke-test.js
+++ test/specs/smoke-test.js
@@ -1,6 +1,7 @@
 import App from '../../page-objects/App'
 import RenderingCards from '../../page-objects/pages/renderingCards'
 import NavBar from '../../page-objects/pages/navBar'
+import HeartApr from '../../page-objects/pages/heartApr'
 
 describe('Smoke test - loading website', () => {
     it('load website', () => {
@@ -9,8 +10,9 @@ describe('Smoke test - loading website', () => {
         RenderingCards.cardIsVisible()
     })
 
-    it('Click on 0% APR verify redericts to Heart', async () => {
-        await (await $('/html/body/div[2]/div/div/div/div/div/div/div/div[2]/div[4]/div/div/div/div[5]/div/div/div/a/img')).click()
+    it('Click on 0% APR verify redericts to Heart', () => {
+        HeartApr.selectHeart()
+        // await (await $('/html/body/div[2]/div/div/div/div/div/div/div/div[2]/div[4]/div/div/div/div[5]/div/div/div/a/img')).click()
     })
 
     it('Click on Nav bar link', () => {
